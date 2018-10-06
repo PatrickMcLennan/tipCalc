41f4516adbc69960eee3996be529037285a57048
Patrick <patrick.a.mclennan@gmail.com>
2018-10-06 19:05:27 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    root: true,
+    extends: 'airbnb-base',
+    parserOptions: {
+        sourceType: 'script',
+    },
+    env: {
+        es6: true,
+        browser: true,
+    },
+};
+
+
