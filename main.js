bfa91953471a60f90d1e00f1b02675f6152d05ea
Patrick <patrick.a.mclennan@gmail.com>
2018-10-07 14:12:37 -0400

    workin'

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const DOM = {
+  inputTotal: document.querySelector('.inputTotal'),
+  inputService: document.querySelector('.service'),
+  inputPeople: document.querySelector('.inputPeople'),
+  button: document.querySelector('button'),
+  responseShare: document.querySelector('.totalShare'),
+  responseComment: document.querySelector('.results__response'),
+  responseBase: document.querySelector('.totalBase'),
+  responseTip: document.querySelector('.totalTip'),
+};
+
+function getTip(service) {
+  let tipPercent;
+  if (service === 'terrible') {
+    tipPercent = 0;
+  } else if (service === 'bad') {
+    tipPercent = 0.1;
+  } else if (service === 'average') {
+    tipPercent = 0.15;
+  } else if (service === 'good') {
+    tipPercent = 0.2;
+  } else {
+    tipPercent = 0.25;
+  }
+  return tipPercent;
+}
+
+function getTotal(total, payees) {
+  let total;
+  total.value
+}
